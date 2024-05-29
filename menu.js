58bb319f0e4c35dc0a56562261a9e440b42bbe42
simonisdev <sagredo83@gmail.com>
2024-05-29 01:09:29 -0400

    actualizacion de requerimientos

--- menu.js
+++ menu.js
@@ -1,8 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('menu.html')
-       .then(response => response.text())
-       .then(data => {
-            document.body.innerHTML += data;
-        })
-       .catch(error => console.error('Error al cargar el menú:', error));
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     fetch('menu.html')
+//        .then(response => response.text())
+//        .then(data => {
+//             document.body.innerHTML += data;
+//         })
+//        .catch(error => console.error('Error al cargar el menú:', error));
+// });
+
+  document.addEventListener('DOMContentLoaded', function() {
+      var template = document.getElementById('menuNavegacion').content;
+      var fragmento = document.importNode(template, true);
+      document.body.appendChild(fragmento);
+  });
